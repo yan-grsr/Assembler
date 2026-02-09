1e6b769f7231b7ca2cab1c78532752f86480ba58
yan-grsr <yan.garrissere@gmail.com>
2026-02-09 22:30:53 +0100

    printing Hello World with jmp

--- hello.asm
+++ hello.asm
@@ -1,20 +1,22 @@
-    section .data
-        msg db 'Hello, World', 0      ; Define a null-terminated string where each char takes one B
+section .data ; Initialize data and contants
+    msg db 'Hello, World', 0
 
-    section .text
-        global _start
+section .text ; section where the program is written
+    global _start
+    
+_start:
+    jmp print ; jump to the adress print
+    jmp _exit ; jump to the adress _exit
 
-    _start:
-        ; to print hello world
-        mov rax, 1      ; syscall number for sys_write (1)
-        mov rdi, 1      ; file descriptor 1 for stdout
-        mov rsi, msg    ; address of the string to output
-        mov rdx, 13     ; length of the string(13 char) Number of bytes to output from address given in rsi
-        syscall         ; invoking the syscall
+print:              ; print Hello World in console
+    mov rax, 1      
+    mov rdi, 1      
+    mov rsi, msg    
+    mov rdx, 13     
+    syscall         
+    
+_exit:              ; exit the program using syscall
+    mov rax, 60    
+    mov rdi, 0     
+    syscall         
 
-        ; to exit the program
-        mov rax, 60     ; syscall number for sys_exit (60)
-        xor rdi, rdi    ; exit code 0. could have wrote "mov rdi, 0"
-        syscall         ; invoking the syscall
-
-        ; clearly we don't have any bss section
